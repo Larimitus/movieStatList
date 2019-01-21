8bf5850027770d7334c439157cc772ee77970a77
Hilario Mendez <hmendezvallejo@unomaha.edu>
2019-01-20 23:31:39 -0600

    Activity 3 with Loop

--- activity3.py
+++ activity3.py
@@ -1,23 +1,30 @@
 print("ISQA 3900 Letter Grade Calculator\n")
 
-print("Enter total number of points earned:\t")
-gradeValue = input()
-gradeValue = float(gradeValue)
+while True:
+    print("Enter total number of points earned: ")
+    gradeValue = input()
+    gradeValue = float(gradeValue)
 
-letterGrade = ""
-if gradeValue > 919.9:
-    letterGrade = "A"
-elif gradeValue > 879.9:
-    letterGrade = "B+"
-elif gradeValue > 819.9:
-    letterGrade = "B"
-elif gradeValue > 779.9:
-    letterGrade = "C+"
-elif gradeValue > 699.9:
-    letterGrade = "C"
-elif gradeValue > 600:
-    letterGrade = "D"
-else:
-    letterGrade = "F"
+    letterGrade = ""
+    if gradeValue > 919.9:
+        letterGrade = "A"
+    elif gradeValue > 879.9:
+        letterGrade = "B+"
+    elif gradeValue > 819.9:
+        letterGrade = "B"
+    elif gradeValue > 779.9:
+        letterGrade = "C+"
+    elif gradeValue > 699.9:
+        letterGrade = "C"
+    elif gradeValue > 600:
+        letterGrade = "D"
+    else:
+        letterGrade = "F"
 
-print("Letter grade: ", letterGrade)
+    print("Letter grade: ", letterGrade)
+    print("\nContinue (y/n)?: ")
+    cont = input()
+    if cont in ['n', 'N']:
+        break
+
+print("\nBye")
